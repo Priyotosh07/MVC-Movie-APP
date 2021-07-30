d284e7c05315bee7d271dce217cf0fad79ac06d9
Priyotosh07 <52592980+Priyotosh07@users.noreply.github.com>
2021-07-30 20:50:37 +0530

    The changes are made to call api methods

---
+++ NewdatabaseMovie/dbo/Stored Procedures/DelteSongs.sql
@@ -0,0 +1,8 @@
+﻿create procedure [dbo].[DelteSongs]    
+(    
+    @Id int     
+)    
+As    
+BEGIN    
+    DELETE FROM Songs WHERE Id=@Id    
+END 
---
+++ NewdatabaseMovie/dbo/Stored Procedures/InsertSongDetails.sql
@@ -0,0 +1,14 @@
+﻿create procedure [dbo].[InsertSongDetails]    
+(    
+    @Songname varchar(50),    
+    @SongAuthor varchar(50),    
+    @SongGenre varchar(50),    
+    @SongRelaeaseDate date    
+)    
+As    
+BEGIN    
+    
+ INSERT INTO Songs(Songname,SongAuthor,SongGenre,SongRelaeaseDate)    
+ VALUES(@Songname,@SongAuthor,@SongGenre,@SongRelaeaseDate)    
+    
+END
